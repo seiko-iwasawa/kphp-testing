e30b867ee8ff76f8189e78b633c127bb51c36fcd
seiko.iwasawa <seiko.iwasawa@gmail.com>
2023-05-10 21:20:36 +0300

    init comment controller

--- forum/routes/web.php
+++ forum/routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comments', [\App\Http\Controllers\CommentController::class, 'index']);
+
+Route::get('/comments/{comment}', [\App\Http\Controllers\CommentController::class, 'show']);
+
+Route::get('/comments/create/comment', [\App\Http\Controllers\CommentController::class, 'create']);
+
+Route::post('/comments/create/comment', [\App\Http\Controllers\CommentController::class, 'store']);
+
+Route::get('/comments/{comment}/edit', [\App\Http\Controllers\CommentController::class, 'edit']);
+
+Route::put('/comments/{comment}/edit', [\App\Http\Controllers\CommentController::class, 'update']);
+
+Route::delete('/comments/{comment}', [\App\Http\Controllers\CommentController::class, 'destroy']);
